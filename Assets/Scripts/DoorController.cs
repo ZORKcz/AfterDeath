fbf73c3701b4053327f4bd0fc0bfcdda3d94be96
ZORKcz <zork7417@gmail.com>
2024-01-30 23:32:20 +0100

    Added: Key and door system

--- Assets/Scripts/DoorController.cs
+++ Assets/Scripts/DoorController.cs
@@ -1,27 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DoorController : MonoBehaviour
 {
     public bool locked;
 
+    private Animator anim;
+
+    [SerializeField] GameObject player;
+
     // Start is called before the first frame update
     void Start()
     {
+        anim = GetComponent<Animator>();
         locked = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float distance = Vector2.Distance(player.transform.position, transform.position);
+
+        if(!locked && distance < 0.5f)
+        {
+            SceneManager.LoadScene(0);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.CompareTag("Key"))
         {
+            anim.SetTrigger("Open");
             locked = false;
         }
     }
@@ -30,6 +42,7 @@ public class DoorController : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Key"))
         {
+            anim.SetTrigger("Closed");
             locked = true;
         }
     }
