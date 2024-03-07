0b73975cc9965364445124325f09e1186f6cfbc0
BanaTomas <banat.05@spst.eu>
2024-03-08 00:54:49 +0100

    Added: powerUps
    pain1.4

---
+++ Assets/armor.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class armor : MonoBehaviour
+{
+    public listOfPowerUpsMethods list;
+    public console consoleScript;
+
+    public bool startCourutineOnce;
+
+    void Start()
+    {
+        startCourutineOnce = true;
+    }
+
+    void Update()
+    {
+        if (list.armorIsActive)
+        {
+            if (startCourutineOnce)
+            {
+                StartCoroutine(armorCO());
+            }
+        }
+    }
+
+    private IEnumerator armorCO()
+    {
+        Debug.Log("Courutine bezi");
+        startCourutineOnce = false;
+        list.OnArmorPickUp();
+        yield return new WaitForSeconds(5);
+        list.OnArmorPickUpReturn();
+        list.armorIsActive = false;
+        startCourutineOnce = true;
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Player")
+        {
+            Collider2D colize = GetComponent<Collider2D>();
+            colize.isTrigger = true;
+            Sprite render = GetComponent<Sprite>();
+            SpriteRenderer gameObject = GetComponent<SpriteRenderer>();
+            gameObject.enabled = false;
+        }
+    }
+}
---
+++ Assets/leatherBoots.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class leatherBoots : MonoBehaviour
+{
+    public listOfPowerUpsMethods list;
+    public console consoleScript;
+
+    public bool startCourutineOnce;
+
+    void Start()
+    {
+        startCourutineOnce = true;
+    }
+
+    void Update()
+    {
+        if (list.speedBonusIsActive)
+        {
+            if (startCourutineOnce)
+            {
+                StartCoroutine(leatherBootsCO());
+            }
+        }
+    }
+
+    private IEnumerator leatherBootsCO()
+    {
+
+        Debug.Log("Courutine bezi");
+        startCourutineOnce = false;
+        list.OnSpeedPickUp();
+        yield return new WaitForSeconds(5);
+        if (!consoleScript.speedHasBeenAdded)
+        {
+            list.OnSpeedPickUpReturn();
+        }
+        list.speedBonusIsActive = false;
+        startCourutineOnce = true;
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Player")
+        {
+            Collider2D colize = GetComponent<Collider2D>();
+            colize.isTrigger = true;
+            Sprite render = GetComponent<Sprite>();
+            SpriteRenderer gameObject = GetComponent<SpriteRenderer>();
+            gameObject.enabled = false;
+        }
+    }
+}
